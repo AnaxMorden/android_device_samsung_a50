f0d166937593e095b2b134a40fa9c42af38aade2
AnaxMorden <79524821+AnaxMorden@users.noreply.github.com>
2021-05-26 13:49:22 +0200

    Changing Lineage_A50.mk

--- lineage_a50.mk
+++ lineage_a50.mk
@@ -21,7 +21,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 # Inherit device configuration
 $(call inherit-product, device/samsung/a50/device.mk)
 #$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/halium.mk)
+# Inherit halium.mk
+$(call inherit-product, vendor/halium/config/halium.mk)
+
 
 # Inherit some common Lineage stuff.
 #$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
